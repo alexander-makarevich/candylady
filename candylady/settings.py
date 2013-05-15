bac2563420e18ae2f871a3105412c6df807f8b90
Alexander Makarevich <alexander.s.makarevich@gmail.com>
2013-05-15 22:39:31 +0700

    Добавлены настройки языка

--- candylady/settings.py
+++ candylady/settings.py
@@ -38,7 +38,11 @@ TIME_ZONE = 'America/Chicago'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
+
+LANGUAGES = (
+    ('ru', gettext('Russian')),
+)
 
 SITE_ID = 1
 
