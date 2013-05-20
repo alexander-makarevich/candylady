20046b2817fb0974b469712ab043710967613693
Alexander Makarevich <alexander.s.makarevich@gmail.com>
2013-05-20 21:46:35 +0700

    Поправил загрузку картинок в lightbox

--- candylady/static/lightbox/js/lightbox.js
+++ candylady/static/lightbox/js/lightbox.js
@@ -48,8 +48,12 @@ lightbox = new Lightbox options
   LightboxOptions = (function() {
 
     function LightboxOptions() {
-      this.fileLoadingImage = 'images/loading.gif';
-      this.fileCloseImage = 'images/close.png';
+      this.fileLoadingImage = $('script[src$="/lightbox.js"]')
+        .attr('src')
+        .replace(/\/lightbox\.js$/, '/../images/loading.gif');
+      this.fileCloseImage = $('script[src$="/lightbox.js"]')
+        .attr('src')
+        .replace(/\/lightbox\.js$/, '/../images/close.png');
       this.resizeDuration = 700;
       this.fadeDuration = 500;
       this.labelImage = "Image";
